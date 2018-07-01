3dfaf3d314d5f08383cb9920c8cf39696f4c592b
GentaMatsumoto <sdlkfj3w0ao2@outlook.jp>
2018-07-02 02:24:29 +0900

    fixed

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     text Faker::Lorem.sentence
-    image File.open("#{Rails.root}/spec/factories/no_image.png")
+    # image File.open("#{Rails.root}/spec/factories/no_image.png")
     user
     group
   end
