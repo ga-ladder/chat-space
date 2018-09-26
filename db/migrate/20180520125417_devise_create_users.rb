58ec7738f35d2571869abaeec718f67561ba6d5a
Genta <8581682+ga-ladder@users.noreply.github.com>
2018-09-26 17:34:01 +0900

    コメント追加

--- db/migrate/20180520125417_devise_create_users.rb
+++ db/migrate/20180520125417_devise_create_users.rb
@@ -37,8 +37,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true #これいらない
+    add_index :users, :reset_password_token, unique: true #これもいらないー＞ のちに:nameにindex貼ってる
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
